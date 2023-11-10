e039ba016e071cbddfbeabcd2a94c2144087353c
Rilee-0320 <go13209@naver.com>
2023-11-11 00:16:52 +0900

    feat: 주문 메뉴 입력 받기

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 import EventDay from './Christmas.js';
+import Receipt from './Receipt.js';
 
 const InputView = {
   async readDate() {
@@ -12,6 +13,18 @@ const InputView = {
     } catch (e) {
       Console.print(e.message);
       return this.readDate();
+    }
+  },
+  async readOrder() {
+    try {
+      const INPUT = await Console.readLineAsync(
+        '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n',
+      );
+      const ORDER = new Receipt(INPUT).getOrder();
+      return ORDER;
+    } catch (e) {
+      Console.print(e.message);
+      return this.readOrder();
     }
   },
 };
---
+++ src/Receipt.js
@@ -0,0 +1,93 @@
+class Receipt {
+  #menu = {
+    양송이수프: 6000,
+    타파스: 5500,
+    시저샐러드: 8000,
+    티본스테이크: 55000,
+    바비큐립: 54000,
+    해산물파스타: 35000,
+    크리스마스파스타: 25000,
+    초코케이크: 15000,
+    아이스크림: 5000,
+    제로콜라: 3000,
+    레드와인: 60000,
+    샴페인: 25000,
+  };
+
+  #order;
+
+  constructor(order) {
+    this.#order = this.#validate(order);
+  }
+
+  #validate(order) {
+    const ORDER_OBJECT = {};
+    const ORDER_LIST = order.split(',');
+
+    if (ORDER_LIST.length === 0) {
+      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+    }
+
+    let hasFood = false;
+    for (const ITEM of ORDER_LIST) {
+      const PARTS = ITEM.split('-');
+
+      if (PARTS.length !== 2) {
+        throw new Error(
+          '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
+        );
+      }
+
+      const [MENU_ITEM, QUANTITY] = PARTS;
+
+      if (!Object.hasOwn(this.#menu, MENU_ITEM)) {
+        throw new Error(
+          '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
+        );
+      }
+
+      const NUMBER_QUANTITY = Number(QUANTITY);
+
+      if (
+        Number.isNaN(NUMBER_QUANTITY) ||
+        NUMBER_QUANTITY < 1 ||
+        !Number.isInteger(NUMBER_QUANTITY)
+      ) {
+        throw new Error(
+          '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
+        );
+      }
+
+      if (ORDER_OBJECT[MENU_ITEM]) {
+        throw new Error(
+          '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
+        );
+      }
+
+      ORDER_OBJECT[MENU_ITEM] = NUMBER_QUANTITY;
+
+      if (!['제로콜라', '레드와인', '샴페인'].includes(MENU_ITEM)) {
+        hasFood = true;
+      }
+    }
+
+    if (!hasFood) {
+      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+    }
+
+    const TOTAL_QUANTITY = Object.values(ORDER_OBJECT).reduce(
+      (cnt, QUANTITY) => cnt + QUANTITY,
+      0,
+    );
+    if (TOTAL_QUANTITY > 20) {
+      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+    }
+    return ORDER_OBJECT;
+  }
+
+  getOrder() {
+    return this.#order;
+  }
+}
+
+export default Receipt;
