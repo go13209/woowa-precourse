40a753cf20bce1e900a08a7cbfea47855892defc
Rilee-0320 <go13209@naver.com>
2023-11-14 00:38:21 +0900

    feat: 주문 메뉴 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,12 @@
-export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
-    }
-    // ...
-}
+import { Console } from '@woowacourse/mission-utils';
+
+const OutputView = {
+  printMenu(ORDER) {
+    Console.print('<주문 메뉴>');
+    Object.entries(ORDER).forEach(([MENU_ITEM, QUANTITY]) => {
+      Console.print(`${MENU_ITEM} ${QUANTITY}개`);
+    });
+  },
+};
+
+export default OutputView;
